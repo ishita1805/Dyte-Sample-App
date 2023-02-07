23fe4c7ce80a1b1d5893fde5ce1a179c0f9852c0
ishita1805 <ishitakabra@dyte.io>
2023-02-07 15:58:04 +0530

    fix: only send notification when hand is raised

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ defineCustomElements();
 const handRaise = document.getElementById('dyte-controlbar-button');
 const searchParams = new URL(window.location.href).searchParams;
 let m;
-const authToken = searchParams.get('authToken');
+const authToken = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE1NTdmOTQ5LWRkMGYtNDZkMy1iNjJlLTczNDhiMTcxNGNkYiIsImxvZ2dlZEluIjp0cnVlLCJpYXQiOjE2NzU3NTE4MzYsImV4cCI6MTY4NDM5MTgzNn0.Zr1l9nzGbhl5a4HjDX9Bjt26K1P0R6b_RasuFIvhtUEPGKipsI3SRIojMOlhrGkWKZ1t3d9qJNndeKN1uHcWYZd_TGeBZAKA-JID8woMiFG8D0jeL2hVQsU9O_pMLeHUHkFt_gO_6lq5YT9WppDGgTrvfYw2h2R15nuQQplz8GA`;
 const roomName = searchParams.get('roomName') || 'eqdqcp-rrrsqm';
 
 if (!authToken) {
@@ -84,11 +84,13 @@ function handleHandRaise() {
   // Toggle UI
   handRaise.classList.toggle('hand-raise-button');
   // Broadcast Message
-  m.participants.broadcastMessage(handRaise.classList.contains('hand-raise-button') ? 'hand-raised' : 'hand-unraised', { 
+  const key = handRaise.classList.contains('hand-raise-button') ? 'hand-raised' : 'hand-unraised'
+  m.participants.broadcastMessage(key, { 
     id: m.self.id,
     name: m.self.name,
   })
   // Send Notification
+  if (key === 'hand-unraised') return;
   sendNotification({
     id: new Date().getTime().toString(),
     message: `Hand Raised by ${m.self.name}`,
